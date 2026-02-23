b308a64dd9f858a1b04573599ee0fc04e8b65453
Ibrohimbek <programming0127@gmail.com>
2026-02-23 08:55:10 +0500

    BRR: Initial commit

---
+++ src/etc/docs.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const docs = () => {};
+
+// Write your comments here....
+
+// Xatoliklarni izlash uchun => ERROR
+// Kelgan joyini izlash uchun => TODO
+// Test & Check qilinadigon joylar uchun => TEST
+// Yakunlangan kodlar uchun => DONE!
+// Savol mavjud bo'lgan joylar uchun => SAVOL
+
+// --------------------------------------------------------
+
+// Create Vite Project
+// npm create vite@latest
